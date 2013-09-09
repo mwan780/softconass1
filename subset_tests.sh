909cc2b044fd1e63653176aa7c74b173b5bec4ef
steven <steven@DesktopServer.(none)>
2013-09-10 09:22:37 +1000

    Restructured git

--- subset_tests.sh
+++ subset_tests.sh
@@ -7,6 +7,7 @@
 PID="`ps -ea | grep $0`"
 tmp_file="$0$(date +%Y-%m-%d_%H:%M:%S)$PID.tmp"
 tmp_file="`echo $tmp_file | cut -f2 -d '/'`"
+	cd test    # Change into Subfolder
 for subset in 1 2 3 4 5 
 do 
 	echo "******************************"
@@ -14,40 +15,33 @@ do
 	echo "*Running Subset $subset Tests*"
 	echo "******************************"
 	echo "******************************"
-	cd test
 	files="`ls subset"$subset"*.pl`"
 	echo $files
 	for answer in $files
 	do 	
 		curr_file_name="`echo $answer | cut -f1 -d '.'`"
 		echo "Converting perl-to-python for $answer"
-		cd ..
-		./perl2python.pl test/$answer > $tmp_file
-		cd test
-		if [ $? ]; then 
+		../perl2python.pl $answer > $tmp_file
+		if [ $? -eq 0 ]; then 
 			echo "_______________________$0 : Error - perl2python output to temp file failed" >&2
 			break;
 		fi 
-
-		diff -q $curr_file_name.py ../$tmp_file
-		if [ $? ]; then 
+		diff -q $curr_file_name.py $tmp_file
+		if [ !$? ]; then 
 			echo "Direct Comparison Source Code Check........................ Failed"
 		else
 			echo "Direct Comparison Source Code Check........................ Passed"
 	    fi
-	    echo "Compilation Test"
-	    cd ..
-	    chmod +x /$tmp_file
-	    ./$tmp_file
-	    cd test
-	    if [ $? ]; then
+	    chmod +x $tmp_file > /dev/null
+	    ./$tmp_file > /dev/null
+	    if [ !$? ]; then
 	    	echo "Compilation Test........................................... Failed"
 	    else
 	    	echo "Compilation Test........................................... Passed"
-		    ../$tmp_file > generatedoutput$tmp_file
+		    ./$tmp_file > generatedoutput$tmp_file
 	    	./$curr_file_name.py > expectedoutput$tmp_file
 	    	diff -q generatedoutput$tmp_file expectedoutput$tmp_file
-	    	if [ $? ]; then
+	    	if [ !$? ]; then
 		     	echo "Program Output Comparison Check............................ Failed"
 		    else
 	    		echo "Program Output Comparison Check............................ Passed"
@@ -56,6 +50,5 @@ do
 	done
 done
 
-rm -f ../*$tmp_file
-rm -f *$tmp_file
-
+rm -f "../*$tmp_file"
+rm -f "*$tmp_file"
