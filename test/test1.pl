66d6f56a75b6431fd70f6a8294f2564ecd8a40bc
steven <steven@DesktopServer.(none)>
2013-09-10 17:22:49 +1000

    Updating Git -

--- test/test1.pl
+++ test/test1.pl
@@ -1,1 +1,20 @@
-#!/bin/perl -w
+#!/usr/bin/perl -w
+
+if ( "1" ) {
+ 	print "1\n";
+}
+
+print "Hello\n" if 1;
+
+if ( "text" ) {
+	print "Equal\n";
+} elsif ( "text2" ) {
+	print "Also Equal\n";
+} else {
+	print "Not Equal\n";
+}
+
+
+
+
+
