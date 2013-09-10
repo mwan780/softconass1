b6bd80b78e1a985c73192ba587dc3d3d106c948b
steven <steven@DesktopServer.(none)>
2013-09-10 15:43:57 +1000

    Updating Git -

--- subset_tests.sh
+++ subset_tests.sh
@@ -4,43 +4,56 @@
 # Author:- Steven Falconieri    
 # *********************************
 # *********************************
+
 program_name="`echo $0 | cut -f2 -d '/'`"
 PID="`ps -ea | grep $0`"
 tmp_file="$program_name$(date +%Y-%m-%d_%H:%M:%S)$PID.tmp"
+echo Running $program_name
 
 # Perform temp file cleanup
-	echo "******************************"
-	echo "******************************"
-	echo "*** Performing Temp Cleanup***"
-	echo "******************************"
-	echo "******************************"
-cd test
+echo "******************************"
+echo "******************************"
+echo "*** Performing Temp Cleanup***"
+echo "******************************"
+echo "******************************"
+cd ./test
 for i in 1 2
 do 
 	old_temps="`ls -1 | egrep -i '$program_name.*\.tmp'`"
 	for temp in $old_temps
 	do
-		rm $temp
+		echo "Trying to delete $temp"
+		rm -f $temp
 		result=$?
 		if [ $result -eq 0 ]; then
-			echo "Old Temp File $temp was deleted"
+			echo "Old Temp File $temp was deleted" 
 		fi
 	done
-	cd ..
 done
-cd test
+
+echo 
+echo 
+echo
 
 
+die() {
+	if [ $1 -ne 0 ]; then
+		echo "$2" >&2
+		exit 1
+	fi
+}
 
-for subset in 1 2 3 4 5 
+
+for subset in 1 2 3 4 5 6 7 8 9 10
 do 
 	echo "******************************"
 	echo "******************************"
 	echo "*Running Subset $subset Tests*"
 	echo "******************************"
 	echo "******************************"
-	files="`ls subset"$subset"*.pl`"
-	echo $files
+
+	files="`ls subset"$subset"*.pl test$subset.pl`"
+	echo "Testing .... $files"
 	for answer in $files
 	do 	
 		curr_file_name="`echo $answer | cut -f1 -d '.'`"
@@ -49,7 +62,7 @@ do
 		result=$?
 		if [ $result -ne 0 ]; then 
 			echo result was $result
-			echo "_______________________$0 : Error - perl2python output to temp file failed" >&2
+			echo "$0 : Error - perl2python output to temp file failed" >&2
 			break;
 		fi 
 		diff -q $curr_file_name.py $tmp_file
@@ -60,14 +73,31 @@ do
 			echo "Direct Comparison Source Code Check.......................$result... Passed"
 	    fi
 	    chmod +x $tmp_file > /dev/null
-	    ./$tmp_file > /dev/null
-	    result=$?
+	    if [ $subset -eq 4 ]; then
+	    	echo -e "$1\n-1" | ./$tmp_file > /dev/null
+			result=$? 
+		else 
+	    	./$tmp_file > /dev/null
+	    	result=$?
+	    fi
+	       
 		if [ $result -ne 0 ]; then
 	    	echo "Compilation Test..........................................$result... Failed"
 	    else
 	    	echo "Compilation Test..........................................$result... Passed"
-		    ./$tmp_file > generatedoutput$tmp_file
-	    	./$curr_file_name.py > expectedoutput$tmp_file
+		  	if [ $subset -eq 4 ]; then
+		  		echo "Generated Output"
+
+		  		echo -e "$1\n-1" | ./$tmp_file > generatedoutput$tmp_file
+	    		die $? "Could not generate output to file"
+	    		echo "Expected Output"
+				echo -e "$1\n-1" | ./$curr_file_name.py > expectedoutput$tmp_file
+				die $? "Could not generate expected ouptut"
+			else 
+	    		./$tmp_file > generatedoutput$tmp_file
+	    		./$curr_file_name.py > expectedoutput$tmp_file
+	    		die $?
+	    	fi
 	    	diff -q generatedoutput$tmp_file expectedoutput$tmp_file
 	    	result=$?
 			if [ $result -ne 0 ]; then
@@ -76,8 +106,23 @@ do
 	    		echo "Program Output Comparison Check...........................$result... Passed"
 	    	fi
 	    fi
+	    echo
+	    echo
 	done
+	echo 
+	echo 
+	echo
+	echo
 done
 
-rm -f "../*$tmp_file"
-rm -f "*$tmp_file"
+
+
+
+
+# Temp File Cleanup
+cd test
+rm $tmp_file
+rm expectedoutput$tmp_file
+rm generatedoutput$tmp_file
+rm -f ../*.tmp
+rm -f *.tmp
