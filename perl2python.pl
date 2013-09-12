1f5978ff8caebc5b17eb9745a8f35acb1d1b99b3
steven <steven@DesktopServer.(none)>
2013-09-12 10:25:11 +1000

    Updating Git -

--- perl2python.pl
+++ perl2python.pl
@@ -5,6 +5,20 @@
 # *********************************
 # *********************************
 
-while(<>) {
-	print;
+
+sub convert_to_python {
+	my (@input) = @_;
+	foreach $line (@input) {
+		print $line;
+	}
+}
+
+foreach $file (@ARGV) {
+	open(PERL, $file) or die "$0: Could not open file : $!\n";
+	convert_to_python(<PERL>);
+}
+
+if ( !($#ARGV >= 0) ) {
+	print "Reading from standard input\n";
+	convert_to_python(<STDIN>);
 }
