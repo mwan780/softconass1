535c1267463ef3d0270adfc41db0f8f95a0acd4d
steven <steven@DesktopServer.(none)>
2013-09-13 21:59:01 +1000

    Updating Git -

--- perl2python.pl
+++ perl2python.pl
@@ -9,8 +9,9 @@
 # ###### Output Functions #######
 # ###############################
 
-sub output_python ($$) {
+sub output_python ( $$ ) {
 	my ($tab_depth, $python) = @_;
+	$python = strip_dollar_signs($python);
 	print "Output:- " if $debug;
 	for($count = 0; $count < $tab_depth; $count++) {
 		print "    ";
@@ -18,7 +19,7 @@ sub output_python ($$) {
 	print "$python";
 }
 
-sub debug ($) {
+sub debug  ( $ )  {
 	my ($message) = @_;
 	print "Debug:- $message\n" if $debug;
 }
@@ -29,67 +30,67 @@ sub debug ($) {
 # ###############################
 # Return:- Boolean 
 
-sub has_opening_brace ($) {
+sub has_opening_brace  ( $ )  {
 	my ($line) = @_;
 	return $line =~ /\{/;
 }
 
-sub has_strictly_closing_brace ($) {
+sub has_strictly_closing_brace  ( $ )  {
 	my ($line) = @_;
 	return $line =~ /^[^\{]*\}/;
 }
-sub has_strictly_opening_brace ($) {
+sub has_strictly_opening_brace  ( $ )  {
 	my ($line) = @_;
 	return $line =~ /^[^\}\{]*\{[^\{\}]*$/;
 }
 
-sub has_both_braces ($) {
+sub has_both_braces  ( $ )  {
 	my ($line) = @_;
 	return $line =~ /^[^\{]*\{[^\}]*\}[^\{\}]$/;	
 }
 
-sub has_closing_then_opening_braces ($) {
+sub has_closing_then_opening_braces  ( $ )  {
 	my ($line) = @_;
 	return $line =~ /^[^\}]*\}[^\{]*\{[^\{\}]$/;
 }
 
-sub is_closing_brace_line ($) {
+sub is_closing_brace_line ( $ ) {
 	my ($line) = @_;
 	return $line =~ /^\s*\}\s*$/;
 }
 
-sub is_opening_brace_line ($) {
+sub is_opening_brace_line ( $ )  {
 	my ($line) = @_;
 	return $line =~ /^\s*\{\s*$/;
 }
 
-sub is_comment_line ($) {
+sub is_comment_line ( $ ) {
 	my ($line) = @_;
 	return $line =~ /^\s*(\#.*)/;
 }
 
-sub reverse_order_if ($) {
+sub reverse_order_if ( $ ) {
 	my ($line) = @_;
 	return $line =~ /\s*?.+?\sif\s*\(?.*\)?\s*$/;
 }
 
-sub else_line ($) {
+sub else_line ( $ ) {
 	my ($line) = @_;
 	return $line =~ /^\s*\}\s*els[ie]f?\s*\{\s*$/;
 }
 
-sub print_line ($) {
+sub print_line ( $ ) {
 	my ($line) = @_;
-	return $line =~ /^\s*print\s*\(?\s*(((\"[^\"]+\"\s*)|(\s*\$\w+\s*))[\.\,])*((\"[^\"]+\"\s*)|(\s*\$\w+\s*))\)?\s*$/;
+	return $line =~ /^\s*print\s*\(?\s*(((\"[^\"]+\"\s*)|(\s*\$\w+\s*))\s*[\.\,\+\*\-\/]\s*)*((\"[^\"]+\"\s*)|(\s*\$\w+\s*))\)?\s*$/;
 }
 
-sub get_print ($) {
+sub get_print ( $ ) {
 	my ($line) = @_;
-	return $1 if $line =~ /^\s*(print\s*\(?\s*(((\"[^\"]+\"\s*)|(\s*\$\w+\s*))[\.\,])*((\"[^\"]+\"\s*)|(\s*\$\w+\s*))\)?)/;
+	return $1 if $line =~ /^\s*(print\s*\(?\s*(((\"[^\"]+\"\s*)|(\s*\$\w+\s*))[\.\,\+\-\/\*])*((\"[^\"]+\"\s*)|(\s*\$\w+\s*))\)?)/;
 	return "";
 }
 
-sub strip_outermost_parentheses ($) {
+sub strip_outermost_parentheses ( $ )  {
 	my ($line) = @_;
 	my $no_parentheses_line = $line;
 	$no_parentheses_line =~ s/^([^\(]*)\(/$1/;
@@ -97,7 +98,7 @@ sub strip_outermost_parentheses ($) {
 	return $no_parentheses_line;
 }
 
-sub strip_outermost_braces ($) {
+sub strip_outermost_braces ( $ ) {
 	my ($line) = @_;
 	my $no_braces_line = $line;
 	# Also replaces first occuring brace with a :
@@ -106,16 +107,35 @@ sub strip_outermost_braces ($) {
 	return $no_braces_line;
 }
 
-sub empty_line ($) {
+sub strip_dollar_signs ( $ ) {
+	my ($line) = @_;
+	my $var_line = $line;
+	$var_line =~ s/\$(\w+)/$1/g;
+	return $var_line;
+}
+
+sub empty_line ( $ ) {
 	my ($line) = @_;
 	return $line =~ /^$/;
+}
+
+sub var_declaration ( $ ) {
+	my ($line) = @_;
+	return $line =~ /\$\w+\s*\=\s*[\"\']?.*[\"\']?\s*$/;	
+} 
+
+sub strip_new_line ( $ ) {
+	my ($line) = @_;
+	$line =~ s/(".*)\\n(.*\".*)$/$1$2/;
+	return $line;
 }
 
 # #######################################
 # #### Recursive Conversion Function ####
 # #######################################
 # Return:- last line number read from
-sub convert_to_python ($$@) {
+sub convert_to_python ( $$@ );
+sub convert_to_python ( $$@ ) {
 	my ($tab_depth, $line_num, @input) = @_;
 	debug("Tab Depth = $tab_depth and input line number = ".$line_num."/".($#input+1));
 	$curr_line = $line_num;
@@ -129,6 +149,7 @@ sub convert_to_python ($$@) {
 			debug("Multiple ($#multiple_lines+1) Lines Detected at line $curr_line") if @multiple_lines > 1;
 			foreach $single_line (@multiple_lines) {
 				chomp $single_line; 					# Is Force added to every line at the end
+				# Striping on output instead --- should be able to delete this. $single_line = strip_dollar_signs($single_line);
 				debug("Input:- $single_line");
 				if(is_closing_brace_line($single_line)) {
 					debug("Line Type:- Closing Brace ");
@@ -140,7 +161,18 @@ sub convert_to_python ($$@) {
 					# Print Comments Directly Out and removing leading spaces
 					debug("Line Type:- Comments ");
 					$single_line =~ /(#.*)/;
-					output_python($tab_depth, "$1\n");
+					$single_line = $1;
+					if($curr_line == 0) {
+						$single_line =~ s/perl -w/python2.7 \-u/;
+					}
+					output_python($tab_depth, "$single_line\n");
+				} elsif (var_declaration($single_line)) { 
+					# Variabe Declaration
+					debug("Line Type:- Variable Declaration ");
+					output_python($tab_depth, "$single_line\n");
+					while($single_line =~ /\$(\w+)/g) {
+						$vars{$line}{$1} = 1;	# This may be useful later otherwise delete it
+					}
 				} elsif ($single_line =~ /\s*if\s*\(?/) {
 					# If Statements
 					debug("Line Type:- If");
@@ -193,7 +225,10 @@ sub convert_to_python ($$@) {
 				} elsif (print_line($single_line)) {
 					debug("Line Type:- Print ");
 					$print_line = strip_outermost_parentheses(get_print($single_line));
-					output_python($tab_depth, "$print_line,\n");
+					$print_line = strip_new_line($print_line);
+					$print_line =~ s/[\"\']\s*\$(\w+)\s*[\"\']/$1 /g;
+
+					output_python($tab_depth, "$print_line\n");
 				} else {
 					debug("Line Type:- Undertermined ");
 					output_python($tab_depth, "# $single_line\n");
